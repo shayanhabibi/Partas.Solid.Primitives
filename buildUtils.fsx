085c2aba73861fd76a134331fed861d1427781a6
shayanhabibi <shayan.habibi01@gmail.com>
2025-07-27 18:57:42 +0800

    chore: tag pattern regex in build scripts
    Signed-off-by: shayanhabibi <shayan.habibi01@gmail.com>

--- buildUtils.fsx
+++ buildUtils.fsx
@@ -35,7 +35,7 @@ module GitCliff =
 
     let createConfig packageName (initialVersion: string option) : Config =
         { Config.Default with
-            Git.TagPattern = "*" + PackageName.toTagSuffix packageName
+            Git.TagPattern = $"([0-9].*)*(?:%s{PackageName.toTagSuffix packageName})$"
             Bump.InitialTag = initialVersion |> Option.defaultValue "0.1.0"
             Changelog.Body =
                 $$$"""{%- macro remote_url() -%}
