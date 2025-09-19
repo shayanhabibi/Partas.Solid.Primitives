ca450ed805507a88249f57ffcfbaff099aa061a0
shayanhabibi <shayan.habibi01@gmail.com>
2025-09-19 16:03:00 +0800

    add: input-mask and rootless bindings
    Signed-off-by: shayanhabibi <shayan.habibi01@gmail.com>

---
+++ Partas.Solid.Primitives.InputMask/Program.fs
@@ -0,0 +1,61 @@
+﻿namespace Partas.Solid.Primitives
+
+open System.Text.RegularExpressions
+open Partas.Solid
+open Fable.Core
+open Browser.Types
+
+[<Erase; AutoOpen>]
+module private InputMaskSpec =
+    [<Erase>]
+    module Spec =
+        [<Literal>]
+        let path = "@solid-primitives/input-mask"
+        [<Literal>]
+        let version = "0.3.1"
+
+[<Erase>]
+module InputMask =
+    [<Erase>]
+    type Selection = Selection of start: float * finish: float
+    [<Erase>]
+    type InputMaskFn = delegate of value: string * selection: Selection -> value: string * selection: Selection
+    [<Erase>]
+    type InputMaskArray = InputMaskArray of U2<string, Regex>[]
+    [<Erase>]
+    type InputMaskRegex = InputMaskRegex of regex: Regex * replacer: (obj -> string)
+    [<Erase; RequireQualifiedAccess>]
+    type InputMask =
+        | Fn of InputMaskFn
+        | Array of InputMaskArray
+        | Regex of InputMaskRegex
+        | String of string
+    [<ImportMember(Spec.path)>]
+    let stringMaskRegExp: Map<string, Regex> = jsNative
+    
+[<AutoOpen; Erase>]
+type InputMask =
+    [<ImportMember(Spec.path)>]
+    static member stringMaskToArray(mask: string, ?regexps: Map<string, Regex>): InputMask.InputMaskArray = jsNative
+    [<ImportMember(Spec.path)>]
+    static member regexMaskToFn(regex: Regex, replacer: obj): InputMask.InputMaskFn = jsNative
+    [<ImportMember(Spec.path)>]
+    static member maskArrayToFn(maskArray: InputMask.InputMaskArray): InputMask.InputMaskFn = jsNative
+    [<ImportMember(Spec.path)>]
+    static member anyMaskToFn(mask: InputMask.InputMask, ?regexps: Map<string, Regex>): InputMask.InputMaskFn = jsNative
+    [<ImportMember(Spec.path)>]
+    static member createInputMask<'MaskEvent>(mask: InputMask.InputMask, ?regexps: Map<string, Regex>): 'MaskEvent -> string = jsNative
+    static member inline createKeyboardInputMask(mask: InputMask.InputMask, ?regexps: Map<string, Regex>) =
+        InputMask.createInputMask<KeyboardEvent>(mask, ?regexps = regexps)
+    static member inline createInputInputMask(mask: InputMask.InputMask, ?regexps: Map<string, Regex>) =
+        InputMask.createInputMask<InputEvent>(mask, ?regexps = regexps)
+    static member inline createClipboardInputMask(mask: InputMask.InputMask, ?regexps: Map<string, Regex>) =
+        InputMask.createInputMask<ClipboardEvent>(mask, ?regexps = regexps)
+    [<ImportMember(Spec.path)>]
+    static member createMaskPattern<'MaskEvent>(inputMask: 'MaskEvent -> string, pattern: string -> string): 'MaskEvent -> string = jsNative
+    static member inline createKeyboardMaskPattern(inputMask: KeyboardEvent -> string, pattern: string -> string) =
+        InputMask.createMaskPattern<KeyboardEvent>(inputMask,pattern)
+    static member inline createInputMaskPattern(inputMask: InputEvent -> string, pattern: string -> string) =
+        InputMask.createMaskPattern<InputEvent>(inputMask,pattern)
+    static member inline createClipboardMaskPattern(inputMask: ClipboardEvent -> string, pattern: string -> string) =
+        InputMask.createMaskPattern<ClipboardEvent>(inputMask,pattern)
---
+++ Partas.Solid.Primitives.Rootless/Program.fs
@@ -0,0 +1,66 @@
+﻿namespace Partas.Solid.Primitives
+
+open System
+open Partas.Solid
+open Fable.Core
+open Fable.Core.JsInterop
+
+[<Erase; AutoOpen>]
+module RootlessSpec =
+    [<Erase>]
+    module Spec =
+        [<Literal>]
+        let path = "@solid-primitives/rootless"
+        [<Literal>]
+        let version = "1.5.1"
+
+[<Erase>]
+module Rootless =
+    /// <summary>
+    /// Callback function for <c>createRootPool</c>. Called when a new root is created.
+    /// </summary>
+    /// <param name="arg">An accessor that returns the argument passed to the <c>RootPoolFunction</c></param>
+    /// <param name="active">An accessor that returns the active state of the root. When <c>false</c>, root is not being used
+    /// and is waiting in the pool to be reused</param>
+    /// <param name="dispose">A function that disposes the root and prevents it from being reused.</param>
+    type RootPoolFactory<'Arg, 'Result> = delegate of arg: Accessor<'Arg> * active: Accessor<bool> * dispose: DisposeCallback -> 'Result
+
+[<AutoOpen; Erase>]
+type Rootless =
+    /// <summary>
+    /// Creates a reactive sub root that will be automatically disposed when its owner does.
+    /// </summary>
+    /// <param name="fn">A function in which the reactive state is scoped</param>
+    /// <param name="owners">Reactive root dependencies - cleanup of ANY of them will trigger sub-root disposal. Defaults to <c>getOwner()</c></param>
+    [<ImportMember(Spec.path)>]
+    static member createSubRoot<'T>(fn: DisposeCallback -> 'T, [<ParamArray>] owners: obj): 'T = jsNative
+    [<ImportMember(Spec.path)>]
+    static member createCallback<'T>(callback: 'T, ?owner: obj): 'T = jsNative
+    [<ImportMember(Spec.path)>]
+    static member createDisposable(fn: DisposeCallback -> unit, [<ParamArray>] owners: obj): DisposeCallback = jsNative
+    [<ImportMember(Spec.path)>]
+    static member createSingletonRoot<'T>(
+        factory: DisposeCallback -> 'T,
+        ?detachedOwner: obj
+        ): Accessor<'T> = jsNative
+    [<ImportMember(Spec.path)>]
+    static member createHydratableSingletonRoot<'T>(factory: DisposeCallback -> 'T): Accessor<'T> = jsNative
+    /// <summary>
+    ///  Creates a pool fo roots, that can be reused. Useful for creating components
+    /// that are mounted and unmounted frequently. When the root is created, it will call
+    /// the <c>factory</c> function with a <c>RootPoolFactory</c> callback. Roots are created
+    /// by calling the returned function, after cleanup they won't be disposed but instead
+    /// put back into the pool to be reused. Next time the function is called, it will
+    /// reuse the root from the pool and update it with the new <c>arg</c>
+    /// </summary>
+    /// <param name="factory">A function that will be called when a new root is created.</param>
+    /// <param name="limit">Defaults to 100. Size of the root pool.</param>
+    [<ImportMember(Spec.path); ParamObject(1)>]
+    static member createRootPool<'Arg, 'Result>(
+            factory: Rootless.RootPoolFactory<'Arg, 'Result>,
+            limit: int
+        ): 'Arg -> 'Result = jsNative
+    [<ImportMember(Spec.path)>]
+    static member createRootPool<'Arg, 'Result>(
+            factory: Rootless.RootPoolFactory<'Arg, 'Result>
+        ): 'Arg -> 'Result = jsNative
